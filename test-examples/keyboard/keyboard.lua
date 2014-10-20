1bc0ff511e6f747db4959b53ee46256e4eaec28d
Martin Lund <martin.lund@keep-it-simple.com>
2014-10-20 15:58:32 +0200

    Improved keyboard test
    Now the keyboard test traverses a list of keys to be succesfully tested.

--- test-examples/keyboard/keyboard.lua
+++ test-examples/keyboard/keyboard.lua
@@ -8,10 +8,27 @@ device.load("keyboard")
 -- tg
 device.keyboard.device = "/dev/input/by-id/usb-04f3_0103-event-kbd"
 -- tg
+-- Keys to test
+keys = {30, 31, 32, 33, 34, 35}
+-- tg
+::input_loop::
+
 print("Waiting for keyboard input event...")
 device.keyboard.wait_key_event()
--- tg
-print("Key " .. device.keyboard.key .. " pressed")
+key = device.keyboard.key
+print("Key " .. key .. " pressed")
+
+-- Test if key has been pressed before
+for i, v in ipairs(keys) do
+    if (v == key) then
+        table.remove(keys, i)
+    end
+end
+
+-- Repeat until all keys have been pressed
+if (table.maxn(keys) ~= 0) then
+    goto input_loop
+end
 -- tg
 device.unload("keyboard")
 disconnect(device)
