01e33353c5bc00f3f3cb0898ac3c01ef75fa28a0
Martin Lund <martin.lund@keep-it-simple.com>
2014-10-20 11:16:46 +0200

    Cleanup keyboard.lua

--- test-examples/keyboard/keyboard.lua
+++ test-examples/keyboard/keyboard.lua
@@ -7,6 +7,8 @@ device = connect("tcp://127.0.0.1:8000")
 device.load("keyboard")
 -- tg
 device.keyboard.device = "/dev/input/by-id/usb-04f3_0103-event-kbd"
+-- tg
+print("Waiting for keyboard input event...")
 device.keyboard.wait_key_event()
 -- tg
 print("Key " .. device.keyboard.key .. " pressed")
